1585f2d744ffcf7b35263f70c2c96aba1b24ce2d
JLP <jcdiem@protonmail.com>
2020-04-16 21:26:07 -0500

    Classes
    Created class templates as an example for Kodi

--- game.py
+++ game.py
@@ -1,7 +1,40 @@
 import pygame
+import levels
 from shapely.geometry import Point, Polygon
-
 #TODO: Get "special objects working, (Enemies?, Level Goal, ETC"
+
+#DrawnRectangle and GameLevel by https://github.com/Jcdiem
+class DrawnRectangle:
+    def __init__(self, corner1, corner2, corner3, conrer4, rgb):
+        """ Create a rectangle for use and storage with the main game
+        Each corner should be an array in [x,y] format
+        RGB should be an array in [r,g,b] format
+        """
+        self.corner1 = corner1
+        self.corner2 = corner2
+        self.corner3 = corner3
+        self.corner4 = corner4 
+        self.rgb = rgb
+    
+    #Returns the RGB array
+    def getRGB(self):
+        return self.rgb
+
+    #Returns corners X,Y values in order from 1-4
+    def getCorners(self):
+        return [self.corner1,self.corner2,self.corner3,self.corner4]
+
+class GameLevel:
+    """Create a game level for parsing
+    id should be the numerical value of the level
+    rectangles should be an array of DrawnRectangle objects
+    """
+    def __init__(self, id, rectangles):
+        self.id = id
+        self.rectangles = rectangles
+    
+    def getRectangles(self):
+        return self.rectangles
 
 pygame.init()
 
@@ -20,6 +53,39 @@ PLAYER_ACCEL_X = 0
 PLAYER_ACCEL_Y = 0
 
 LEVEL_NUM = 0
+
+#START EXAMPLE CODE
+
+#Name of level      new lvl  id  start of rectangle array
+exampleDebugLevel = GameLevel(0,[#Open bracket for start of levels array
+    #Main Floor
+    #New Rect       Corner1    Corner2     Corner3      Corner4    R   G  B
+    DrawnRectangle([0, 460], [1024, 460], [1024, 576], [0, 576], [255,128,0]),
+    #Platform
+    DrawnRectangle([[0, 300], [300, 300], [300, 350], [0, 350]],[255, 128, 255]),
+    #Right Wall
+    DrawnRectangle([1000, 460], [1000, 0], [1024, 0], [1024, 460],[255, 128, 0])
+    ]#End rectangles
+)#End making level
+
+#Less cluttered version
+exampleDebugNoClutter = GameLevel(0,[
+    DrawnRectangle([0, 460], [1024, 460], [1024, 576], [0, 576], [255,128,0]), #Main Floor
+    DrawnRectangle([[0, 300], [300, 300], [300, 350], [0, 350]],[255, 128, 255]), #Platform
+    DrawnRectangle([1000, 460], [1000, 0], [1024, 0], [1024, 460],[255, 128, 0]) #Right Wall
+    ])
+
+BETTER_LEVEL_LIST = [
+    exampleDebugLevel,
+    exampleDebugNoClutter
+]
+
+def betterLevelIterate():
+    curLevel = BETTER_LEVEL_LIST[0]
+    for dRectangle in curLevel.getRectangles:
+        print(dRectangle.getCorners)
+
+#END EXAMPLE CODE
 
 LEVELS_COLLISION_MAP = [
     [#DEBUG LEVEL
